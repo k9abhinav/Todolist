12e88f8efaf91b2d32a412b1060d5df34318ff63
k9abhinav <k9abhinava@gmail.com>
2023-11-22 20:37:41 +0530

    Added Vercel CLI for deploying

--- app/page.js
+++ app/page.js
@@ -12,10 +12,10 @@ const page = () => {
     setdesc("");
   };
   const deleteHandler = (i) => {
-    let copytask = [...mainTask]
-    copytask.splice(i,1)
-    setmainTask(copytask)
-  }
+    let copytask = [...mainTask];
+    copytask.splice(i, 1);
+    setmainTask(copytask);
+  };
   let rendertask = <h2>No Task available</h2>;
   if (mainTask.length > 0) {
     rendertask = mainTask.map((t, i) => {
@@ -24,9 +24,14 @@ const page = () => {
           <div className="flex align-middle justify-evenly my-5">
             <h3 className="text-3xl font-bold">{t.title}</h3>
             <h4 className="text-xl font-semibold">{t.desc}</h4>
-            <button onClick={ () => {
-              deleteHandler(i)
-            }} className="rounded font-semibold bg-red-700 text-white w-1/12">Delete</button>
+            <button
+              onClick={() => {
+                deleteHandler(i);
+              }}
+              className="rounded font-semibold bg-red-700 text-white w-1/12"
+            >
+              Delete
+            </button>
           </div>
         </li>
       );
@@ -38,7 +43,10 @@ const page = () => {
       <h1 className="bg-black text-white font-bold p-5 text-2xl text-center">
         MY SIMPLE TODO LIST
       </h1>
-      <form onSubmit={submitHandler} className="flex flex-col m-auto items-center py-3 w-full ">
+      <form
+        onSubmit={submitHandler}
+        className="flex flex-col m-auto items-center py-3 w-full "
+      >
         <input
           className="px-2 py-2 m-8 border-4 border-zinc-800 rounded w-1/4"
           placeholder="Enter Title here"
@@ -57,7 +65,7 @@ const page = () => {
             setdesc(e.target.value);
           }}
         ></input>
-        <button className="bg-black text-center font-semibold text-white rounded px-3 py-2 m-8 w-1/4" >
+        <button className="bg-black text-center font-semibold text-white rounded px-3 py-2 m-8 w-1/4">
           Add Task
         </button>
       </form>
